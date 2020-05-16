1fe83fd15700c7559d88ca46a05e8e1f2b8ff264
Andrei Chabokha <achabokha@users.noreply.github.com>
2020-05-16 15:11:50 -0700

    reading optional param

--- src/utils.py
+++ src/utils.py
@@ -8,14 +8,20 @@ import os
 def get_params():
 
     parser = argparse.ArgumentParser(description="Parameters")
-    parser.add_argument("config", default='./up-zonk.yaml')
     parser.add_argument("model")
+    parser.add_argument("--config",  default='./up-zonk.yaml', required=False)
     args = parser.parse_args()
+
+    filename_w_ext = os.path.basename(args.model)
+    filename, file_extension = os.path.splitext(filename_w_ext)
+
+    model_file_name = args.model if file_extension == "yaml" else filename + ".yaml"
+    print(model_file_name)
 
     with open(args.config) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 
-    model_path = os.path.join(config["up"], args.model)
+    model_path = os.path.join(config["up"], model_file_name)
     with open(model_path) as f:
         model = yaml.load(f, Loader=yaml.FullLoader)
 
--- start.zsh
+++ start.zsh
@@ -3,4 +3,4 @@
 echo "Starting nodemon for templates debuging..."
 echo "It will monitor *.py and *.mustache files and restart python3"
 
-nodemon --exec "python3 " ./src/genie.py up-zonk.yaml product.yaml  --ext py,mustache,yaml
+nodemon --exec "python3 " ./src/genie.py product --ext py,mustache,yaml
