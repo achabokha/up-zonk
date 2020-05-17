d191ca9ab32e90dc75192037b145222f13fcab5a
Andrei Chabokha <achabokha@users.noreply.github.com>
2020-05-17 14:50:26 -0700

    order-item added

---
+++ out/client/src/app/modules/catalog/order-item-action/order-item-action.component.ts
@@ -0,0 +1,10 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-OrderItemAction',
+    templateUrl: './OrderItemAction.component.html',
+    styleUrls: ['./OrderItemAction.component.scss'],
+})
+export class OrderItemActionComponent {
+    constructor() {}
+}
--- out/client/src/app/services/service.ts
+++ out/client/src/app/services/service.ts
@@ -1,31 +1,31 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { Product } from '@models/product.interface';
+import { OrderItem } from '@models/order-item.interface';
 
 @Injectable({
     providedIn: 'root',
 })
-export class ProductService {
+export class OrderItemService {
     constructor(private httpClient: HttpClient) {}
 
-    getAll(): Observable<Product[]> {
-        return this.httpClient.get<Product[]>('rest/products');
+    getAll(): Observable<OrderItem[]> {
+        return this.httpClient.get<OrderItem[]>('rest/order_items');
     }
 
-    create(item: Product) {
-        return this.httpClient.post('rest/products', item);
+    create(item: OrderItem) {
+        return this.httpClient.post('rest/order_items', item);
     }
 
-    read(id): Observable<Product>  {
-        return this.httpClient.get<Product>('rest/products/${id}');
+    read(id): Observable<OrderItem>  {
+        return this.httpClient.get<OrderItem>('rest/order_items/${id}');
     }
 
-    update(id, item: Product) {
-        return this.httpClient.put('rest/products/${id}', item);
+    update(id, item: OrderItem) {
+        return this.httpClient.put('rest/order_items/${id}', item);
     }
 
     delete(id) {
-        return this.httpClient.delete('rest/products/${id}');
+        return this.httpClient.delete('rest/order_items/${id}');
     }
 }
