c7c9273f23fe54dc01d61898b4f6252eac11f49c
Andrejs Saboha <andrejssaboha@gmail.com>
2020-05-24 15:58:07 +0100

    Customer base template

--- tests/catalog/client/src/app/modules/catalog/catalog-routing.module.ts
+++ tests/catalog/client/src/app/modules/catalog/catalog-routing.module.ts
@@ -5,12 +5,18 @@ import { Routes, RouterModule } from "@angular/router";
 import { ProductDetailsComponent } from "./product-details/product-details.component";
 import { CatalogComponent } from "./catalog.component";
 import { ProductListComponent } from "./product-list/product-list.component";
+import { CustomerListComponent } from "./customer/customer-list/customer-list.component";
+import { CustomerEditComponent } from "./customer/customer-edit/customer-edit.component";
+import { CustomerDetailsComponent } from "./customer/customer-details/customer-details.component";
 
 const routes: Routes = [
-    { path: "", redirectTo: "products", pathMatch: 'full' },
+    { path: "", redirectTo: "products", pathMatch: "full" },
     { path: "products", component: ProductListComponent },
     { path: "products/:productName", component: ProductDetailsComponent },
     { path: "products/:productName/edit", component: ProductEditComponent },
+    { path: "customer", component: CustomerListComponent },
+    { path: "customer/:customerName", component: CustomerDetailsComponent },
+    { path: "customer/:customerName/edit", component: CustomerEditComponent },
 ];
 
 @NgModule({
--- tests/catalog/client/src/app/modules/catalog/catalog.module.ts
+++ tests/catalog/client/src/app/modules/catalog/catalog.module.ts
@@ -7,11 +7,18 @@ import { CatalogComponent } from "./catalog.component";
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
+
+import { CustomerComponent } from "./customer/customer-action/customer-action.component";
+import { CustomerListComponent } from "./customer/customer-list/customer-list.component";
+import { CustomerDetailsComponent } from "./customer/customer-details/customer-details.component";
+import { CustomerEditComponent } from "./customer/customer-edit/customer-edit.component";
+
 import { ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-    declarations: [CatalogComponent, ProductListComponent, ProductDetailsComponent, ProductEditComponent],
+    declarations: [CatalogComponent, ProductListComponent, ProductDetailsComponent, ProductEditComponent
+    , CustomerComponent, CustomerListComponent, CustomerDetailsComponent, CustomerEditComponent],
     imports: [CommonModule, MaterialModule, ReactiveFormsModule, FlexLayoutModule, CatalogRoutingModule],
 })
 export class CatalogModule {}
